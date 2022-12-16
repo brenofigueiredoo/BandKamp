34602371521c21b8cdff69f39b4c3f875cd219ee
unknown <brenos93@gmail.com>
2022-12-16 13:53:23 -0300

    deploy settings  render

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -15,6 +15,7 @@ from datetime import timedelta
 import os
 import dj_database_url
 import dotenv
+from django.core.management.utils import get_random_secret_key
 
 dotenv.load_dotenv()
 
@@ -26,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
